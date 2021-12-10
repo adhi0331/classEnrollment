d029baf589885b14c3389409e52345e0ebd9a2e6
Adhithya Ananthan <adhi03@Adhithyas-MacBook-Pro.local>
2021-12-09 18:30:52 -0800

    nn

--- classEnrollment/bot.py
+++ classEnrollment/bot.py
@@ -7,13 +7,15 @@ class InstaBot:
         self.driver = webdriver.Safari()
         self.driver.get("https://www.instagram.com/")
         sleep(5)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
+        self.driver.find_element_by_name("username").send_keys(username)
         sleep(5)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
+        self.driver.find_element_by_name("password").send_keys(pw)
         sleep(5)
-        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element('//button[@type="submit"]').click()
+        sleep(5)
+        self.driver.close()
         sleep(20)
 
-InstaBot("adhi_ananthan", "Lamboaven03")
+InstaBot("", "")
 
 
