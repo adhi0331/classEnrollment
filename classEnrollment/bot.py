c12f46c78d5b7729d778e91dd7caed806fa2f649
adhi0331 <adhi03.ananthan@gmail.com>
2021-12-11 18:55:13 -0800

    created login function for bot

--- classEnrollment/bot.py
+++ classEnrollment/bot.py
@@ -5,17 +5,26 @@ from time import sleep
 class InstaBot:
     def __init__(self, username, pw):
         self.driver = webdriver.Safari()
+        self.username = username
+        self.pw = pw
+    
+    def login(self):
         self.driver.get("https://www.instagram.com/")
-        sleep(5)
-        self.driver.find_element_by_name("username").send_keys(username)
-        sleep(5)
-        self.driver.find_element_by_name("password").send_keys(pw)
-        sleep(5)
-        self.driver.find_element('//button[@type="submit"]').click()
+        sleep(1)
+        self.driver.find_element_by_name("username").click()
+        self.driver.find_element_by_name("username").send_keys(self.username)
+        sleep(1)
+        for index,letter in enumerate(self.pw):
+            self.driver.find_element_by_name("password").click()
+            self.driver.find_element_by_name("password").send_keys(letter)
+        sleep(1)
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         sleep(5)
         self.driver.close()
-        sleep(20)
 
-InstaBot("", "")
+username = input("Enter username: ")
+pw = input("Enter password: ")
+steve = InstaBot(username, pw)
+steve.login()
 
 
