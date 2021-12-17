381acb76d1357c219dbbdbef79a664eef7b4d0a2
adhi0331 <adhi03.ananthan@gmail.com>
2021-12-16 18:22:51 -0800

    Able to get to the dm page

--- classEnrollment/bot.py
+++ classEnrollment/bot.py
@@ -9,7 +9,7 @@ class InstaBot:
         self.pw = pw
     
     def login(self):
-        self.driver.get("https://www.instagram.com/")
+        self.driver.get("https://www.instagram.com")
         sleep(1)
         self.driver.find_element_by_name("username").click()
         self.driver.find_element_by_name("username").send_keys(self.username)
@@ -19,12 +19,21 @@ class InstaBot:
             self.driver.find_element_by_name("password").send_keys(letter)
         sleep(1)
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
-        sleep(5)
-        self.driver.close()
+        #Maybe add a part to click on the save info button
+    #A function that will read each new DM and return a list of all requests to be made
+    def checkDM(self):
+        sleep(2)
+        self.driver.get("https://www.instagram.com/direct/inbox/")
+
+        sleep(10)
+    
+    #Create a function that will iterate through the request list, ensures that each request is valid, if valid then run request
 
 username = input("Enter username: ")
 pw = input("Enter password: ")
-steve = InstaBot(username, pw)
-steve.login()
+stewie = InstaBot(username, pw)
+stewie.login()
+stewie.checkDM()
+stewie.driver.close()
 
 
